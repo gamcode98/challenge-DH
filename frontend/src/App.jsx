257aad076293d836817b74bb5d4aa019d0d209a5
Gabriel <gamcode98@gmail.com>
2024-01-30 22:14:29 -0300

    add react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import { Route, Routes } from 'react-router-dom'
+
 function App () {
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path='/' element={<div className='underline'>Home</div>} />
+    </Routes>
   )
 }
 
