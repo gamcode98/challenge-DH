722e42ea55c1a92117b4f6be07f98e5d8c23076c
Gabriel <gamcode98@gmail.com>
2024-02-14 17:33:57 -0300

    add handle errors and metadata response in controllers

--- backend/src/controllers/applicant.controller.js
+++ backend/src/controllers/applicant.controller.js
@@ -1,13 +1,26 @@
 const { applicantService } = require('../services/applicant.services')
 
 const findAll = async (req, res) => {
-  const content = await applicantService.findAll()
+  try {
+    const data = await applicantService.findAll()
 
-  res.status(200).json({
-    statusCode: res.statusCode,
-    message: 'Aspirantes cargados exitosamente',
-    content
-  })
+    res.status(200).json({
+      meta: {
+        error: false,
+        count: data.length,
+        statusCode: res.statusCode,
+        url: '/applicants',
+        message: 'Aspirantes cargados exitosamente'
+      },
+      data
+    })
+  } catch (error) {
+    console.error('Error: ', error)
+    res.status(500).json({
+      statusCode: res.statusCode,
+      message: 'Server error'
+    })
+  }
 }
 
 const applicantController = {
--- backend/src/controllers/professions.controller.js
+++ backend/src/controllers/professions.controller.js
@@ -1,13 +1,26 @@
 const { professionsService } = require('../services/professions.services')
 
 const findAll = async (req, res) => {
-  const content = await professionsService.findAll()
+  try {
+    const data = await professionsService.findAll()
 
-  res.status(200).json({
-    statusCode: res.statusCode,
-    message: 'Profesiones cargados exitosamente',
-    content
-  })
+    res.status(200).json({
+      meta: {
+        error: false,
+        count: data.length,
+        statusCode: res.statusCode,
+        url: '/professions',
+        message: 'Profesiones cargados exitosamente'
+      },
+      data
+    })
+  } catch (error) {
+    console.error('Error: ', error)
+    res.status(500).json({
+      statusCode: res.statusCode,
+      message: 'Server error'
+    })
+  }
 }
 
 const professionsController = {
