72ab1fe76513c7cbff1414621c535ed5d96a58a7
Carolina Maria de los Angeles Cortez <112413900+ChiaroCoure@users.noreply.github.com>
2024-03-03 22:29:25 -0300

    Update Header.jsx

--- frontend/src/pages/header/Header.jsx
+++ frontend/src/pages/header/Header.jsx
@@ -1,4 +1,5 @@
 import './css/header.css'
+import { Link } from "react-router-dom";
 import { Outlet } from 'react-router-dom'
 
 function Header () {
@@ -7,9 +8,9 @@ function Header () {
       <header className='fixed top-0 left-0 w-full flex justify-between items-center p-4 bg-white bg-opacity-70 z-10'>
         <nav className='hidden md:block'>
           <ul className='flex'>
-            <li className='mr-4'>Inicio</li>
-            <li className='mr-4'>Lista</li>
-            <li>Agregar Aplicante</li>
+            <Link to="/"><li className='mr-4'>Inicio</li></Link>
+            <Link to="/applicants"><li className='mr-4'>Lista</li></Link>
+            <Link to="/create-applicant"><li>Agregar Aplicante</li></Link>
           </ul>
         </nav>
         <div>
